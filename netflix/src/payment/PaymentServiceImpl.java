ebdb4146a3d5b64dba52d65f1516e1535b4ef972
2hwooo727@gmail.com <2hwooo727@gmail.com>
2016-08-01 18:32:39 +0900

    member & payment

--- netflix/src/payment/PaymentServiceImpl.java
+++ netflix/src/payment/PaymentServiceImpl.java
@@ -3,9 +3,10 @@ package payment;
 import java.util.List;
 import java.util.Map;
 
+import member.MemberDAO;
+
 public class PaymentServiceImpl implements PaymentService{
-	private PaymentDAO dao = PaymentDAO.getInstance();
-	
+	private PaymentDAO pDao = PaymentDAO.getInstance();
 	private static PaymentServiceImpl instance = new PaymentServiceImpl();
 	
 	private PaymentServiceImpl() {
@@ -15,12 +16,12 @@ public class PaymentServiceImpl implements PaymentService{
 	}
 	@Override
 	public List<PaymentBean> list() {
-		 return dao.selectAll();
+		 return pDao.selectAll();
 	}
 	@Override
 	public List<PaymentBean> findBy(String keyword) {
 		String[] format = keyword.split(":");
-		return dao.selectBy(format[0],format[1]);
+		return pDao.selectBy(format[0],format[1]);
 	}
 	@Override
 	public Map<Integer, PaymentBean> map() {
@@ -28,26 +29,23 @@ public class PaymentServiceImpl implements PaymentService{
 	}
 	@Override
 	public PaymentBean findByPk(int paySeq) {
-		return dao.selectByPk(paySeq);
+		return pDao.selectByPk(paySeq);
 	}
-	
 	@Override
 	public boolean insert(MemberPaymentCard pcmBean) {
 		boolean result = false;
-		if(dao.insert(pcmBean)==1){
-		/*	if(dao.update(pcmBean)==1){
-				result = true;				
-			}*/
+		if(pDao.insert(pcmBean)==1){
+			result = true;
 		}
 		return result;
 	}
 	@Override
 	public int update(PaymentBean payBean) {
-		return dao.update(payBean);
+		return pDao.update(payBean);
 	}
 	@Override
 	public int delete(int paySeq) {
-		return dao.delete(paySeq);
+		return pDao.delete(paySeq);
 	}
 	@Override
 	public boolean checkCard(CardBean cardBean) {
